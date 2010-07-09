080e2f1c65470ae59b2b40c22d58cef9a839a946
John Britton <public@johndbritton.com>
2010-07-09 02:58:59 -0700

    the basics are all here and it works now

---
+++ README.md
@@ -0,0 +1,12 @@
+# Clickable Phone Numbers
+A Firefox extension built with JavaScript in the [Jetpack Add-on Builder](http://builder.mozillalabs.com) that makes all phone numbers on the web into links and automatically connects your mobile or land line phone to the number using [Twilio](http://www.twilio.com).
+
+# Files
+app.js is the Jetpack JavaScript file.
+clickable-phone-numbers.xpi is an installable Firefox extension.
+
+# Background
+
+Built at [Mozilla Summit 2010](http://wiki.mozilla.org/Summit2010) in a very short time, this could be done much better. It works!
+
+[John Britton](http://www.johndbritton.com)
---
+++ app.js
@@ -0,0 +1,38 @@
+var tabs = require("tabs");
+var simpleStorage = require("simple-storage");
+
+tabs.onReady.add(function(tab){
+  var phoneNumberRegex = /(\d{3})-(\d{3})-(\d{4})/g;
+  var body = tab.contentDocument.body.innerHTML;
+  body = body.replace(phoneNumberRegex, '<a class="dialtwilio" href="#">$&</a>');
+  tab.contentDocument.body.innerHTML = body;
+  tab.contentDocument.addEventListener("click", function(event){
+    if (event.target.className == "dialtwilio") {
+      initiateCall(event.target.innerHTML);
+    }
+  }, true);
+});
+
+function initiateCall(to) {
+  if (!simpleStorage.storage.sid) {
+    simpleStorage.storage.sid = tabs.activeTab.contentWindow.prompt("Please enter your Twilio Account Sid.");
+  }
+  if (!simpleStorage.storage.token) {
+    simpleStorage.storage.token = tabs.activeTab.contentWindow.prompt("Please enter your Twilio Auth Token.");
+  }
+  if (!simpleStorage.storage.from) {
+    simpleStorage.storage.from = tabs.activeTab.contentWindow.prompt("Please enter your phone number.");
+  }
+
+  require("request").Request({
+    url: "https://" + simpleStorage.storage.sid + ":" + simpleStorage.storage.token + "@api.twilio.com/2008-08-01/Accounts/" + simpleStorage.storage.sid + "/Calls",
+    content: {
+      Caller: simpleStorage.storage.from,
+      Called: simpleStorage.storage.from,
+      Url: "http://twimlets.com/forward?PhoneNumber=" + to,
+    },
+    onComplete: function() {
+       console.log(this.response.text);
+    }
+  }).post();
+}
